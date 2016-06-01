d51aee29a6e5d7131ccaebdc1b8d6056a3c34cf0
Cameron Morrow <cmorrow2@csub.edu>
2016-06-01 16:32:29 -0700

    Update roseP.h

--- roseP.h
+++ roseP.h
@@ -7,9 +7,11 @@ void init_sounds();
 void release_sounds();
 void load_sounds();
 void play_sounds(int soundOption);
+void play_sounds(int soundOption, int loop);
 void bubblez(int radius, float red, float green, float blue);
 void pressR(Game *g);
 void render_maze(Game *g, GLuint mazeTexture, Ppmimage *mazeImage);
 void maze_collision(Game *g);
+void getVolume(float V);
 
 #endif
