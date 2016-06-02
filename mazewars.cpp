73168ae510de38fd3eea93346b3001aab2ee219e
David Vaughan <dvaughan@csub.edu>
2016-06-02 01:26:37 -0700

    autoupdate

--- mazewars.cpp
+++ mazewars.cpp
@@ -610,7 +610,7 @@ int check_keys(XEvent *e)
 
 void physics(Game *g)
 {
-	check_gblock_collision(g->Player_1, g, 0.1, 0.1);
+	//check_gblock_collision(g->Player_1, g, 0.1, 0.1);
 
 	struct timespec bt;
 	clock_gettime(CLOCK_REALTIME, &bt);
